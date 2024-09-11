d9c1071fa815377ef85c920904de7f9407c96a45
Tanvir <zishantanvir05@gmail.com>
2024-09-11 23:18:24 +0600

    add 5 problem solve

---
+++ problem-5.js
@@ -0,0 +1,18 @@
+function waitingTime(waitingTimes, serialNumber) {
+  if (Array.isArray(waitingTimes) !== true || typeof serialNumber !== "number") {
+    return "Invalid Input";
+  }
+
+  let sum = 0;
+  for (let waiting of waitingTimes) {
+    sum = waiting + sum;
+  }
+  let average = Math.round(sum / waitingTimes.length);
+  let her_serial = serialNumber - 1;
+  let before_her = her_serial - waitingTimes.length;
+  let time_need = before_her * average;
+
+  return time_need;
+}
+
+console.log(waitingTime(7, 10));
